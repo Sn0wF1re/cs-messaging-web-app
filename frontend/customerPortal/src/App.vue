6503748dca32cd759d977ca123d1583d6a23e390
Sn0wF1re <mokayaclinton34@gmail.com>
2023-10-22 16:29:02 +0300

    Create App.vue file

---
+++ frontend/customerPortal/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/send-message">Send Message</router-link> | 
+      <router-link to="/conversation-history">Conversation History</router-link>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #6610f2;
+}
+</style>
