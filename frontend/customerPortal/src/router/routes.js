4d95814c74c0ba7d794a7fca72c115f48fb547a1
Sn0wF1re <mokayaclinton34@gmail.com>
2023-10-22 16:28:12 +0300

    Create routes for the different views

---
+++ frontend/customerPortal/src/router/routes.js
@@ -0,0 +1,9 @@
+import SendMessage from '../views/SendMessage.vue';
+import ConvoHistory from '../views/ConvoHistory.vue';
+
+const routes = [
+  { path: '/send-message', component: SendMessage },
+  { path: '/conversation-history', component: ConvoHistory },
+];
+
+export default routes;
